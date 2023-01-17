a446ad06a7e169371b5d1f4ecdb2db28d49f70d6
Isaac Hussain <isaaclearns58@gmail.com>
2023-01-17 07:22:46 +0300

    adds deleteItem functionality

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const btnSubmit = document.querySelector('.btn-submit');
 const btnClearList = document.querySelector('.btn-clear-list');
-const deleteItem = document.querySelector('.delete-item');
 
 btnSubmit.addEventListener('click', () => {
     const textInputValue = document.querySelector('.text-input').value; 
     const todoList = document.querySelector('.todo-list');
     let todoItem = document.createElement("li");
+    let btnDeleteItem = document.createElement("span");
+    let btnDone = document.createElement("a");
+    btnDeleteItem.innerText = "X";
     todoItem.innerText = textInputValue;
     todoItem.classList.add("todo-item");
+    btnDeleteItem.classList.add("btn-delete-item");
+    btnDeleteItem.setAttribute("onclick", "deleteItem(event)");
+    todoItem.appendChild(btnDeleteItem);
     todoList.appendChild(todoItem);
     document.querySelector('.text-input').value = '';
 })
@@ -18,10 +23,8 @@ btnClearList.addEventListener('click', () => {
     })
 }); 
 
-todoList.addEventListener('click', (e) => {
-    
-    console.log(e.target);
-    if (e.target.tagName === "li"){
-        todoList.removeChild(e.target);
-    }
-});
+function deleteItem(e){
+    const parentElement = e.target.parentNode;
+    parentElement.remove();
+}
+
